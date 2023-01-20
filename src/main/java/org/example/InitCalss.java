806aceb65721d4452f81c035d49135bc1bb8f50e
Gősi Dominik <gosidominik@lltk.local>
2023-01-20 09:08:45 +0100

    Initial commit

---
+++ src/main/java/org/example/InitCalss.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class InitCalss {
+    private String text;
+
+    InitCalss() {
+        this.text = "bootcamp";
+    }
+
+    private void whoAmI() {
+        System.out.println(this.text);
+    }
+}
